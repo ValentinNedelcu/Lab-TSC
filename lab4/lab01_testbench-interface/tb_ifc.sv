161cd59fe6370e7d5856942bc667bd2999ace756
Valentin <valentin.nedelcu02@gmail.com>
2022-03-28 11:50:13 +0300

    lab5

--- lab4/lab01_testbench-interface/tb_ifc.sv
+++ lab4/lab01_testbench-interface/tb_ifc.sv
@@ -5,7 +5,7 @@
 interface tb_ifc (input logic clk);
   //timeunit 1ns/1ns;
 
-  // user-defined types are defined in instr_register_pkg.sv
+  
   import instr_register_pkg::*;
 
   // ADD CODE TO DECLARE THE INTERFACE SIGNALS
@@ -17,36 +17,20 @@ interface tb_ifc (input logic clk);
   instruction_t  instruction_word;
 
  modport tb(
-   clocking cb
+   clocking cb;
+ );
 
-  output  logic          clk,
- output load_en,
- output reset_n,
- output operand_a,
- output operand_b,
- output opcode,
- output write_pointer,
- output read_pointer,
+ 
+ clocking cb@(clk);
+   output  logic          clk,
+ output load_en;
+ output reset_n;
+ output operand_a;
+ output operand_b;
+ output opcode;
+ output write_pointer;
+ output read_pointer;
  input instruction_word
- );
-
- modport dut(
-   input  logic          clk,
- input  logic          load_en,
- input  logic          reset_n,
- input operand_t      operand_a,
- input operand_t      operand_b,
- input opcode_t       opcode,
- input address_t      write_pointer,
- input address_t      read_pointer,
- output instruction_t  instruction_word
- );
-
- clocking cb@(posedge clk);
-  input reset_n;
-  input reset_en;
-  load_read_pointer;
-  endclocking
 
   endinterface: tb_ifc
 
